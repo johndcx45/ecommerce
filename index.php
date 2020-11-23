c7bc94090cc3328793c36cc85a276dfdad579c83
John <johndcx45@gmail.com>
2020-11-23 13:25:14 -0300

    Added SQL support to the project

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 
 require_once("vendor/autoload.php");
 
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($results);
-
+    $sql = new Hcode\DB\Sql();
+    $results = $sql->select("SELECT * FROM tb_users");
+    echo json_encode($results);
 });
 
 $app->run();
